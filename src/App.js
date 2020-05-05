6b73e33541e484382385110a166989f5e5d1749e
Nithesh Dumpala <nitheshdumpala@Nitheshs-MacBook-Pro.local>
2020-05-06 03:49:45 +0530

    adding hero image  container

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, NavLink, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import Route from 'react-router-dom/Route';
-import home  from './pages/home';
-import about  from './pages/about';
-import contact  from './pages/contact';
-import errorPage  from './pages/errorPage';
+import home  from './pages/Home';
+import about  from './pages/About';
+import contact  from './pages/Contact';
+import errorPage  from './pages/ErrorPage';
 
 function App() {
   return (
     <Router>
         <div className="App">
-        <ul>
-            <li>
-                <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-                <NavLink to="/about">ABout</NavLink>
-            </li>
-            <li>
-                <NavLink to="/contact">Contact</NavLink>
-            </li>
-        </ul>
         <Switch>
             <Route path="/" exact strict component={home}/>
             <Route path="/about" component={about}/>
---
+++ src/containers/HeroContainer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Container, Row, Button } from 'react-bootstrap';
+
+
+const HeroContainer = React.memo(() => {
+    return (
+        <div className="heroImage">
+            <div className="layer">
+                <Container fluid>
+                    <Row className="text-size-sm flex-right">
+                        <Button className="margin-horizontal-sm" href="/" variant="primary" size="lg">Home</Button>
+                        <Button className="margin-horizontal-sm" href="/about" variant="primary" size="lg">About</Button>
+                        <Button className="margin-horizontal-sm" href="/contact" variant="primary" size="lg">Contact</Button>
+                    </Row>
+                    <Row className="flex-column text-size-md flex-center">
+                        <div>Hello, I&#x27;m <span className="text-color">Nithesh Dumpala.</span></div>
+                        <div>I&#x27;m a Front-end developer </div>
+                        <div className="margin-vertical-sm">
+                            <Button href="/contact" variant="primary" size="lg">Contact me</Button>
+                        </div>
+                    </Row>
+                </Container>
+            </div>
+        </div>
+    );
+});
+export default HeroContainer;
